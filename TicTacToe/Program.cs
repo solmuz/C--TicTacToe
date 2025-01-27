b347b0d74d486c8d25304ebb207a27ecb6141de7
Daniel Mz <74578477+solmuz@users.noreply.github.com>
2025-01-27 16:49:37 -0600

    Update Program.cs

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace TicTacToe{
+    class Program{    
+        static void Main(string[] args)
+        {
+            TicTacToe ticTacToe = new TicTacToe();
+            while (true){
+                ticTacToe.DisplayBoard();
+                ticTacToe.PlayerMove();
+                if(ticTacToe.IsBoardFull())
+                {
+                    ticTacToe.DisplayBoard();
+                    Console.WriteLine("Yall both failed the vibe check, it's a tie womp womp.");
+                    break;
+                }
+                if(ticTacToe.CheckWinner()){
+                    ticTacToe.DisplayBoard();
+                    Console.WriteLine($"Congratz!, player {ticTacToe.currentPlayer} passed the vibe check.");
+                    break;
+                }
+                
+                ticTacToe.SwitchPlayer();
+            }
+        }
+    }
+}
